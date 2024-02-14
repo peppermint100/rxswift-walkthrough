5fb030cd64506f624cccf9ea2016f7937a8d0b58
peppermint100 <krpeppermint100@gmail.com>
2024-02-14 16:48:00 +0900

    RxMemo #1 cocoapod 설정

---
+++ RxMemo/RxMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxMemo
+//
+//  Created by peppermint100 on 2/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
