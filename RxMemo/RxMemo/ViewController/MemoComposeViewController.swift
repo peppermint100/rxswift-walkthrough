d708cf0ab9ef013b80efd25372600078bf67a01c
peppermint100 <krpeppermint100@gmail.com>
2024-02-15 14:35:23 +0900

    RxMemo - bind를 통한 메모작성

--- RxMemo/RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/RxMemo/ViewController/MemoComposeViewController.swift
@@ -6,15 +6,48 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
         
     var viewModel: MemoComposeViewModel!
     
+    @IBOutlet var cancelButton: UIBarButtonItem!
+    @IBOutlet var saveButton: UIBarButtonItem!
+    @IBOutlet var contentTextView: UITextView!
+    
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        saveButton.rx.tap.throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        contentTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
     }
 }
